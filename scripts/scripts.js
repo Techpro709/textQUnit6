3b9673999ef784fbb6c7b7198b1a6ae656bff150
TechPro709 <61668117+Techpro709@users.noreply.github.com>
2023-02-13 14:47:23 -0800

    add info

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,87 @@
 var titlecard = [
     {
-        title1: ``,
-        title2: ``,
-        name: ``
+        title1: `Animation`,
+        title2: `Text Question Unit 6`,
+        name: `Zakariah Y.`
     }
 ];
 
 var mycards = [
     {
         txt: {
-            txt: ``,
-            classes: ''
+            tag: `h4`,
+            txt: `Choose one of Newton’s Three Laws of Motion OR Newton’s Law of Gravity and explain what that Law states (in your own words) and why it is important and relevant to the world `,
+            classes: `floating`,
+        },
+        txt2: {
+            tag: `p`,
+            txt: `When an object is in motion unless it is stopped or changed, it will stay in motion. It is important because otherwise there technically we would fly forever`,
+            classes: `lead`,
         }
-
     },
     {
         txt: {
-            txt: ``,
-            classes: ''
+            tag: `h4`,
+            txt: `Discuss ways to give an object motion in animation, and describe the ways that objects move in animation.`,
+            classes: `floating`,
+        },
+        txt2: {
+            tag: `p`,
+            txt: `You might move the background and keep the object in the center to show it is moving, or you could keep the background still and let the object move over the screen.`,
+            classes: `lead`
         }
-    }
+    },
+    {
+        txt: {
+            tag: `h4`,
+            txt: `The unit discusses the importance of a character’s eyes in expressing their feelings and emotions. Imagine that you have a character with only black dots for eyes.`,
+        },
+        txt2: {
+            tag: `h5`,
+            txt: `How might this impact your character or story? What other ways could you express this character’s emotions?`,
+            classes: `floating`
+        },
+
+        txt3: {
+            tag: `p`,
+            txt: `If a character had dots for eyes, then you couldn’t show as well the emotions for example eyes tearing up might be hard and so is the pleading eyes animation.`,
+            classes: `lead`
+        }
+    },
+    {
+        txt: {
+            tag: `h4`,
+            txt: `How might observing the world and people around you improve your animation skills?`,
+        },
+        txt2: {
+            tag: `h5`,
+            txt: `Explain`,
+            classes: `floating`
+        },
+
+        txt3: {
+            tag: `p`,
+            txt: `You might see different behaviors just like how people walk.`,
+            classes: `lead`
+        }
+    },
+    {
+        txt: {
+            tag: `h4`,
+            txt: `From everything that you have learned about movement in animation, what aspect of movement do you think would be the most difficult to implement and why?`,
+        },
+        txt2: {
+            tag: `h5`,
+            txt: `How could you potentially master this aspect of motion in animation? Explain.`,
+            classes: `floating`
+        },
+
+        txt3: {
+            tag: `p`,
+            txt: `Well probably the proper gravity in the world. Kind of hard for me simulate.`,
+            classes: `lead`
+        }
+    },
 ];
 
 
@@ -52,16 +114,20 @@ function loadtitlecard() {
 
 
 function load() {
-    for (var card of mycards) {
-        document.getElementById('main').innerHTML += `
+    for (var card in mycards) {
+        var cardText = [];
+
+        var obj = mycards[card];
+        for (var text in obj) {
+            console.log(obj);
+            cardText += `
+            <${obj[text].tag} class="${obj[text].classes}">${obj[text].txt}</${obj[text].tag}>`;
+        }
+        document.getElementById("main").innerHTML += `
         <div class="bg-text" data-aos="flip-down" data-aos-duration="1000">
-        <p class="lead ${card.txt.classes}">
-          ${card.txt.txt}
-        </p>
-      </div>
-    `
+        ${cardText}
+        </div>`;
     }
 
-
-    document.getElementById('main').innerHTML += `<div>Built by ProtonZ With &hearts;</div>`
+    document.getElementById("main").innerHTML += `<div>Built by ProtonZ With &hearts;</div>`;
 }
